7e12cf9bc6e887217cf5dda8965cf1c48ba8e1f3
paulstey <paulstey@gmail.com>
2017-09-22 13:39:55 -0700

    change a lot

--- performance_optim/type_system.jl
+++ performance_optim/type_system.jl
@@ -1,26 +1,49 @@
 # type_system.jl
+using DataFrames
 
-
-function count_letters(x)
-    res = Dict()
-    n = length(x)
-    for i = 1:n
-        res[x[i]] = get(res, x[i], 0) + 1
+# Type-Stable functions
+function strip_first_char(s)
+    if isna(s)
+        res = NA
+    else
+        res = s[2:end]
     end
     res
 end
 
 
-function count_letters2(x)
-    res = Dict{Char, Int}()
-    n = length(x)
-    for i = 1:n
-        res[x[i]] = get(res, x[i], 0) + 1
-    end
+function strip_first_char2(s::String)
+    res = s[2:end]
     res
 end
 
 
-a = rand(['a', 'b', 'c', 'd'], 50_000_000)
-@time count_letters(a)
-@time count_letters2(a)
+function strip_first_char2(s::NAtype)
+    res = NA
+    res
+end
+
+
+function strip_char_column!(v)
+    n = length(v)
+    for i = 1:n
+        v[i] = strip_first_char(v[i])
+    end
+end
+
+
+function strip_char_column2!(v)
+    n = length(v)
+    for i = 1:n
+        v[i] = strip_first_char2(v[i])
+    end
+end
+
+n = 10_000_000
+words = rand(Any["a man", "a plan", "a canal", "panama", NA], n)
+nums = randn(n)
+df1 = DataFrame(a = words, b = nums)
+
+@time strip_char_column2!(df1[:, 1])
+
+@code_warntype strip_first_char2("no")
